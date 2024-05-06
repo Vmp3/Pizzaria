e0e97106f1a4721c80ee2675bc00436078bf79d1
PedrohFolster <pedro.santos13@alunos.sc.senac.br>
2024-05-06 20:42:09 -0300

    Add new main page component
    This commit adds a new main page component to the application. The component includes a fullscreen video background and an overlaid welcome text. The video and text are styled with CSS to provide an engaging visual experience for users. This component will be the first page seen by visitors when accessing the application.

--- pizzaria/src/App.js
+++ pizzaria/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Menu from './Components/Menu/Menu';
+import Principal from './Components/Principal/Principal';
 
 function App() {
   return (
     <div className="App">
       <Menu />
+      <Principal />
     </div>
   );
 }
---
+++ pizzaria/src/Components/Principal/Principal.js
@@ -0,0 +1,39 @@
+import React, { useRef, useEffect } from "react";
+import "./Principal.css";
+
+const Principal = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current
+        .play()
+        .then(() => {
+          // Vídeo foi iniciado
+        })
+        .catch((error) => {
+          // O navegador não permitiu a reprodução automática
+          console.error("Erro ao iniciar a reprodução automática:", error);
+        });
+    }
+  }, []);
+
+  return (
+    <div className="video-container">
+      <video ref={videoRef} className="fullscreen-video" loop>
+        <source
+          src={process.env.PUBLIC_URL + "/Util/Pizza.mp4"}
+          type="video/mp4"
+        />
+        Seu navegador não suporta o elemento de vídeo.
+      </video>
+      <div className="text-overlay">
+        <p className="welcome">Welcome</p>
+        <h2>Bem-vindo à nossa pizzaria!</h2>
+        <p>Não perca essa chance de ter um belo jantar! Realize seu pedido abaixo</p>
+      </div>
+    </div>
+  );
+};
+
+export default Principal;
