d2661ac1866291790d7cc28b585519e5e7e1d6a8
PedrohFolster <pedro.santos13@alunos.sc.senac.br>
2024-05-29 19:51:12 -0300

    Alter ListarPizza.css

--- font-end/pizzaria/src/Pages/Pizza/ListarPizza/ListarPizza.js
+++ font-end/pizzaria/src/Pages/Pizza/ListarPizza/ListarPizza.js
@@ -20,15 +20,17 @@ function ListarPizzas() {
   }, []);
 
   return (
-    <div className="grid-container">
+    <div className="container">
       <h2>Listagem de Sabores</h2>
       {erro && <p>{erro}</p>}
       <div className="grid">
         {pizzas.map((pizza) => (
           <div key={pizza.id} className="grid-item">
-            {pizza.imagem && <img src={pizza.imagem} alt={pizza.titulo} className="pizza-image" />}
+            <div className="pizza-image-container">
+              {pizza.imagem && <img src={pizza.imagem} alt={pizza.titulo} className="pizza-image" />}
+            </div>
             <h3>{pizza.titulo}</h3>
-            <span>{pizza.descricao}</span>
+            <p>{pizza.descricao}</p>
           </div>
         ))}
       </div>
